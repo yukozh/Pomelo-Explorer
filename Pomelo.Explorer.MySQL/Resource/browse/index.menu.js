3751c4475cb1fccb6a5b424efad21aed089d1d17
Yuko Zheng <hi@yuko.me>
2020-03-21 19:15:38 +0800

    Implement toggle database

--- Pomelo.Explorer.MySQL/Resource/browse/index.menu.js
+++ Pomelo.Explorer.MySQL/Resource/browse/index.menu.js
@@ -3,20 +3,25 @@
     qv.createView('/mysql/getdatabases/' + app.getMenu().instance)
         .fetch((data) => {
             app.getMenu().databases = data;
+            for (var i = 0; i < data.length; ++i) {
+                app.getMenu().toggled.push(false);
+            }
         });
 };
 
 component.data = function () {
     return {
-        active: {
-            db: null,
-            action: null,
-            detail: null
-        },
+        toggled: [],
         instance: null,
         databases: [],
         tables: {},
     };
+};
+
+component.watch = {
+    toggled: function () {
+        app.getMenu().$forceUpdate();
+    }
 };
 
 component.methods = {
